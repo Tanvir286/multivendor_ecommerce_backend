c5e1188142021c9a9db9cce01b4621ec4de70792
Tanvir286 <tanvirdiu200@gmail.com>
2025-07-19 08:24:58 +0600

    create  coupon

--- src/coupon/coupon.service.ts
+++ src/coupon/coupon.service.ts
@@ -1,11 +1,11 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Category } from 'src/entity/catagory.entity';
 import { Coupon } from 'src/entity/coupon.entity';
 import { Product } from 'src/entity/product.entity';
 import { Store } from 'src/entity/store.entity';
 import { User } from 'src/entity/user.entity';
-import { Repository } from 'typeorm';
+import { Not, Repository } from 'typeorm';
 import { CreateCouponDto } from './dto/create-coupon.dto';
 
 @Injectable()
@@ -30,37 +30,29 @@ export class CouponService {
 
     ){}
 
+    /*<========================================>
+         🏳️   Create Coupon  Start    🏳️
+    ===========================================>*/
+
     async create(createCouponDto: CreateCouponDto, userId: number): Promise<any> {
 
         const { couponCode, 
                 discountType, 
                 discountAmount,
+                scope,
                 storeId, 
                 productId, 
                 categoryId,
                 expireAt } = createCouponDto;
             
 
-        // Check if the user exists
-        const user = await this.userRepository.findOne({ where: { id: userId } });
-        if (!user) {
-            throw new Error(`User with ID ${userId} not found`);
-        }
-
         // Check if the store exists and belongs to the user
         const store = await this.storeRepository.findOne({ where: { id: storeId, storeOwner: { id: userId } } });
         if (!store) {
-            throw new Error(`Store with ID ${storeId} not found or does not belong to user ${userId}`);
+            throw new NotFoundException(`Store with ID ${storeId} not found or your are not onwer `);
         }
 
-        // Create a new coupon
-        const coupon = this.couponRepository.create({
-            ...createCouponDto,
-            store,
-            productId,
-            categoryId,
-        });
-
+      
         return this.couponRepository.save(coupon);
     }
 
--- src/coupon/dto/create-coupon.dto.ts
+++ src/coupon/dto/create-coupon.dto.ts
@@ -1,5 +1,5 @@
 import { ApiProperty } from "@nestjs/swagger";
-import { IsDateString, IsEnum, IsInt, IsNotEmpty, IsNumber, IsOptional, IsString } from "class-validator";
+import { IsDateString, IsEnum, IsInt, IsNotEmpty, IsNumber, IsOptional, IsString, Min } from "class-validator";
 import { DiscountType, ScopeType } from "src/entity/coupon.entity";
 
 
@@ -15,15 +15,16 @@ export class CreateCouponDto {
   @IsNotEmpty()
   discountType: DiscountType;
 
-  @ApiProperty({ description: "Discount amount", example: 50.5 })
+  @ApiProperty({ description: "Discount amount(amount for Fixed, percentage for Percentage)", example: 50})
   @IsNumber()
+  @Min(0)
   @IsNotEmpty()
   discountAmount: number;
 
   @ApiProperty({ description: "Scope of the coupon", enum: ScopeType, example: "Product" })
   @IsEnum(ScopeType)
   @IsNotEmpty()
-  scope: ScopeType;
+  scope: ScopeType; 
 
   @ApiProperty({ description: "Store ID",  example: 101 })
   @IsInt()
